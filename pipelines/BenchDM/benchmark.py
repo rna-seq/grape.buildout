1bcfa4bd74a70fce7e4217c8fa6eca56d642e398
Maik R��der <maikroeder@gmail.com>
2012-03-31 22:20:58 +0000

    =every 5 secs

--- pipelines/BenchDM/benchmark.py
+++ pipelines/BenchDM/benchmark.py
@@ -163,7 +163,7 @@ def main():
         while 1:
             args = poll(interval)
             refresh_window(*args)
-            interval = 6
+            interval = 5
     except (KeyboardInterrupt, SystemExit):
         pass
 
